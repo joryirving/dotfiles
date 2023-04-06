d9e5c8a5199ea9e9043c860058604866d389162b
Jory Irving <46251616+LilDrunkenSmurf@users.noreply.github.com>
2023-04-06 13:20:59 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -136,10 +136,10 @@ function initialize_shell() {
   fi
   #echo "Installing Homebrew"
   #/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo "Installing oh-my-zsh"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  echo "Installing powerlevel10k"
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+  #echo "Installing oh-my-zsh"
+  #sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  #echo "Installing powerlevel10k"
+  #git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 }
 
 function main() {
